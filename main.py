14bea99d0b3f02bbbfa95181440a7324c54dc4cd
LucasM212 <43610120+LucasM212@users.noreply.github.com>
2019-01-08 08:57:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
+x1=100
+x2=50
+y1=430
+y2=50
+y = 0
+x = 240
 
+
+def setup():
+    size(640, 480)
+
+def draw():
+    background(0)
+    global x1,y1,x2,y2
+    global x, y
+    fill(155)
+    rect(x1, y1, x2, y2)
+    fill(255)
+    ellipse(x, y, 25, 25) 
+    if y >= 640:
+        y = 0
+    y += 1 
+    
+        
+ 
+def keyReleased():
+    global x1, y1, x2, y2
+    if (key==CODED):
+        if (keyCode == LEFT):
+            x1=x1-20
+        elif (keyCode == RIGHT):
+            x1=x1+20
