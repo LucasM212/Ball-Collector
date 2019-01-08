8054229c2f2086205854d5e1774c9b39bbe6d2bd
LucasM212 <43610120+LucasM212@users.noreply.github.com>
2019-01-08 09:38:52 -0500

    Update development.py

--- development.py
+++ development.py
@@ -4,13 +4,15 @@ y1=430
 y2=25
 y = 0
 x = 240
-
+score = 0
 
 def setup():
     size(640, 480)
 
 def draw():
+    global score
     background(0)
+    text(score, 20, 255)
     global x1,y1,x2,y2
     global x, y
     fill(155)
@@ -22,6 +24,7 @@ def draw():
     y += 1 
     if (x >= x1 and x <= x + 100) and (y >= y1 and y <= y1 + 50):
         y = 0
+        score += 10
     
     
     
