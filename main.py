de55027bffd08ed90f84563dff68c08edbc81ef6
LucasM212 <43610120+LucasM212@users.noreply.github.com>
2019-01-11 09:03:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
+import random
 x1=100
-x2=50
+x2=100
 y1=430
-y2=50
+y2=25
 y = 0
 x = 240
-
+score = 0
 
 def setup():
     size(640, 480)
 
 def draw():
+    global score
     background(0)
+    text(score, 20, 255)
     global x1,y1,x2,y2
     global x, y
     fill(155)
@@ -19,7 +22,14 @@ def draw():
     ellipse(x, y, 25, 25) 
     if y >= 640:
         y = 0
-    y += 1 
+    y += 4 
+    if (x >= x1 and x <= x1 + 100) and (y >= y1 and y <= y1 + 50):
+        y = 0
+        x = random.randint(0, 480)
+        score += 10
+    if y >= 480:
+        sys.exit()
+    
     
         
  
